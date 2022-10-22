a0e2d02aab73fc2f0d855182b28f02faa00a1f9b
alvaroblancoperez98 <alvaro.blanco22@campuscamara.es>
2022-10-22 17:59:22 +0200

    DOM y algunas funciones

--- javascript/script.js
+++ javascript/script.js
@@ -1,67 +1,86 @@
+//función validar
 function validar(){
-    var form= document.form;
     var dni_regex = /^(\d{8})([A-Z])$/;
     var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var phone_regex =  /^\+?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/;
     var phone_regex2 =  /^\+?([0-9]{3})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/;
+    //recogemos los datos gracias al DOM
+    var nombre = document.getElementById("nombre").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var email = document.getElementById("correo").value;
+    var telefono = document.getElementById("telefono").value;
+    var dni = document.getElementById("dni").value;
+    var iban = document.getElementById("iban").value;
+    var nickname = document.getElementById("nickname").value;
+    var fechanac = document.getElementById("fechanac").value;
 
-    if(form.nombre.value == 0){
+
+
+    //validación nombre
+    if(nombre == 0){
         alert("El nombre esta vacio");
-        form.nombre.value="";
-        form.nombre.focus();
         return false;
     } else {
         console.log("nombre perfecto");
     }
-
-    if(form.apellidos.value == 0){
+    //validación apellidos
+    if(apellidos == 0){
         alert("Los apellidos estan vacios");
     } else {
         console.log("apellidos bien");
     }
-
-    if(form.dni.value.match(dni_regex)){
+    
+    //validación dni
+    if(dni.match(dni_regex)){
         console.log("dni correcto");
     } else {
         alert("formato dni incorrecto");
     }
-
-    if(form.correo.value.match(email_regex)){
+    //validacion email
+    if(email.match(email_regex)){
         console.log("correo correcto");
     } else {
         alert("formato correo incorrecto");
     }
-
-    if(form.telefono.value.match(phone_regex, phone_regex2)){
+    
+    //validación movil
+    if(telefono.match(phone_regex, phone_regex2)){
         console.log("telefono correcto");
     }else {
         alert("error en el telefono");
     }
     
-    if(form.nickname.value == 0){
+    //validación nickname
+    if(nickname == 0){
         alert("Rellene su nombre de usuario");
-        form.nickname.value="";
-        form.nickname.focus();
         return false;
         
     }else {
         console.log("nickname correcto");
     }
 
-    if(form.fechanac.value !== 0){
+
+    //validación fecha nacimiento
+    if(fechanac !== 0){
         console.log("fecha de nacimiento correcta");
     }else {
         alert("Rellene su fecha de nacimiento");
     }
 
-    if (isValidIBANNumber(form.iban.value) == true) {
-        console.log("iban perfe");
+
+    //validación IBAN
+    if (!iban.match(/([a-zA-Z]{2})\s*\t*(\d{2})\s*\t*(\d{4})\s*\t*(\d{4})\s*\t*(\d{4})\s*\t*(\d{2})\s*\t*(\d{10})/g)) {
+        console.log("iban no es valido");
     } else {
-        alert("casi crack");
+        alert("iban perfecto");
     }
+    document.getElementById("form").addEventListener("click", function(event){
+        event.preventDefault()
+        return true;
+      });
 }
 
-
+//función para vaciar los campos al darle al boton reiniciar.
 function vaciar(){
     form = document.form;
     form.nombre.value = "";
@@ -73,3 +92,4 @@ function vaciar(){
     form.nickname.value = "";
     form.fechanac.value = "";
 }
+
